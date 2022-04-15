3948f9e681626a142741e0296e771a37757edb2c
Clad Vesto <auronold@gmail.com>
2022-04-15 15:42:08 -0400

    added working kpiRatio calculation

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var resourceNode = 0 // button
-var upgradedMobs = 0 // button
-var killedPlayers = 0 // button
-var bankedLoot = 0 // button
-var openWorldChests = 0 // button
-var escapedGank = 0 // button
-var deathCount = 0 // button
-var mightProgression = 0 // Starting Might vs Ending Might, user entered
-var factionPoints = 0 // Starting FP vs Ending FP, user entered
-var totalAchievements = 0
+var resourceNode = 0; // button
+var upgradedMobs = 0; // button
+var killedPlayers = 0; // button
+var bankedLoot = 0; // button
+var openWorldChests = 0; // button
+var escapedGank = 0; // button
+var deathCount = 0; // button
+var mightProgression = 0; // Starting Might vs Ending Might, user entered
+var factionPoints = 0; // Starting FP vs Ending FP, user entered
+var totalAchievements = 0;
 var kpiDeathRatio // Calculated from total achievements divided by deaths
 
 function resNodeEarned() {
@@ -17,20 +17,34 @@ function resNodeEarned() {
     console.log("Total Achievements " + totalAchievements)
     document.getElementById("resource").innerHTML = "Plentiful Resource Nodes: " + resourceNode;
     document.getElementById("achievements").innerHTML = "Total Achievements " + totalAchievements;
-    document.getElementById("kpidRatio").innerHTML = "KPI to Death Ratio: " + kpiDeathRatio;
+    ach2deathRatio()
 }
+
 function upgradedMobsKilled() {
     upgradedMobs++
     totalAchievements++
-    //console.log(upgradedMobs);
+    console.log("Upgraded Mobs: " + upgradedMobs);
     console.log("Total Achievements " + totalAchievements)
     document.getElementById("upgradedMobs").innerHTML = "Upgraded Mobs Killed: " + upgradedMobs;
     document.getElementById("achievements").innerHTML = "Total Achievements " + totalAchievements;
-    document.getElementById("kpidRatio").innerHTML = "KPI to Death Ratio: " + kpiDeathRatio;
+    ach2deathRatio()
 }
 function youDie() {
     deathCount++
-    //console.log("Deaths " + deathCount);
+    console.log("Deaths: " + deathCount);
     document.getElementById("deaths").innerHTML = "Deaths: " + deathCount;
-    document.getElementById("kpidRatio").innerHTML = "KPI to Death Ratio: " + kpiDeathRatio;
+    ach2deathRatio()
+}
+
+function ach2deathRatio() {
+    if (deathCount == 0) {
+        document.getElementById("kpidRatio").innerHTML = "KPI to Death Ratio: " + totalAchievements;
+        console.log("IF IS TRUE");
+    } else {
+        kpiDeathRatio = totalAchievements / deathCount;
+        document.getElementById("kpidRatio").innerHTML = "KPI to Death Ratio: " + kpiDeathRatio;
+        console.log("IF IS FALSE");
+    }
+
+    console.log(kpiDeathRatio)
 }
